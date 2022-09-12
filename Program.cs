4acffbe5de2cc17e4c165da2000f5808792ebb4c
mahajasl <111913907+mahajasl@users.noreply.github.com>
2022-09-12 10:04:30 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RecruitCatmahajasl.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RecruitCatmahajaslContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RecruitCatmahajaslContext") ?? throw new InvalidOperationException("Connection string 'RecruitCatmahajaslContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
